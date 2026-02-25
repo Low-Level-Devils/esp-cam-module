d3f65a1bd7cdccf7affb07ce465290bba29805e5
Josh Walker <walker11710@gmail.com>
2026-02-24 17:16:05 -0700

    Add basic AP init

--- main/main.c
+++ main/main.c
@@ -1,6 +1,54 @@
-#include <stdio.h>
+#include <string.h>
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "esp_log.h"
+#include "nvs_flash.h"
+#include "esp_mac.h"
+
+#define SSID "TEST_SSID"
+#define PASS "money"
+#define MAX_STA_CONN 4
+
+static const char *TAG = "wifi_ap";
+
+void wifi_init_softap(void) {
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
+
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+    esp_netif_create_default_wifi_ap();
+
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+
+    wifi_config_t wifi_config = {
+        .ap = {
+            .ssid = SSID,
+            .ssid_len = strlen(SSID),
+            .password = PASS,
+            .max_connection = MAX_STA_CONN,
+            .authmode = WIFI_AUTH_WPA_WPA2_PSK,
+        },
+    };
+
+    if (strlen(PASS) == 0) {
+        wifi_config.ap.authmode = WIFI_AUTH_OPEN;
+    }
+
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_AP));
+    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_AP, &wifi_config));
+    ESP_ERROR_CHECK(esp_wifi_start());
+
+    ESP_LOGI(TAG, "Wi-Fi AP started. SSID:%s password:%s",
+             SSID, PASS);
+}
 
 void app_main(void)
 {
-
+    wifi_init_softap();
 }
